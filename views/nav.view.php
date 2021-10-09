a764b723b1b2d8c55e6380dc003d090e57035eed
Matheus Galvao <matheusrigonigalvao@gmail.com>
2021-10-09 15:48:42 -0300

    first

---
+++ views/nav.view.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="assets/Logo.svg" alt="" width="60" height="60">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <button type="button" aria-current="page" class="button buttonSelected">Senhas</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="button">Meus Dados</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="button">Sair</button>
+                </li>
+
+        </div>
+    </div>
+</nav>
