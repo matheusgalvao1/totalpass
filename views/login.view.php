ee8ae9f9aa2834221cd09b2d923a029e8283d54c
Mairon Lucas <mairon@alunos.utfpr.edu.br>
2021-10-14 17:53:10 -0300

    login view

--- views/login.view.php
+++ views/login.view.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>TotalPass</title>
 </head>
-<body >
-    <div class = "panel panel-default">
-        <div class = "panel-body">
+<body>
+    <div>
+        <div>
             <label >Email</label>
             <br />
             <input type="text">
