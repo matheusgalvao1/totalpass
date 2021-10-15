6afb76f671ca918b79c5cca6f501150ff4324f1a
Mairon Lucas <mairon@alunos.utfpr.edu.br>
2021-10-14 21:40:11 -0300

    login bunitino uiui

--- views/login.view.php
+++ views/login.view.php
@@ -5,35 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TotalPass</title>
-    <style type="text/css">
-        section{
-            height: 90vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .container{
-            border: 10px solid gray;
-            max-width: 600px;
-            margin: auto;
-            width: 100%;
-            height: 50vh;
-
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/login.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-    <section>
-        <div class="container">
-            <label>Email</label><br />
-            <input type="text"><br />
-            <label >Senha</label><br />
-            <input type="password">
+    <div class="fundo">
+        <div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-2">
+                    <h3>Acesso ao TotalPass</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Seu email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Sua senha *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div class="form-group">
+                            <a href="#" class="ForgetPwd" value="Login">Ainda sem conta? <strong>Cadastrar</strong></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
 </body>
 </html>
