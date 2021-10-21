42879647148d53d92c9b4bb6228f773c07c277e8
Matheus Galvao <matheusrigonigalvao@gmail.com>
2021-10-21 20:05:17 -0300

    contaBox css

--- views/home.view.php
+++ views/home.view.php
@@ -9,14 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/buttons.css">
-    <style>
-        .contaBox {
-            border-radius: 20px;
-            border: 5px solid royalblue;
-            padding: 20px;
-            width: 500px;
-        }
-    </style>
+    <link rel="stylesheet" href="css/contaBox.css">
 </head>
 
 <div class="container" style="padding-top: 30px; padding-bottom: 30px">
