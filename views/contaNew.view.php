50e35f0b825bec953e6c8a71efa10f98802b22b3
Matheus Galvao <matheusrigonigalvao@gmail.com>
2021-10-22 10:12:52 -0300

    melhorei botao gerar senha

--- views/contaNew.view.php
+++ views/contaNew.view.php
@@ -8,7 +8,7 @@
     <title>Nova Conta</title>
 </head>
 
-<div class="contaBox" style="border: 5px solid green; padding-right: 0px">
+<div class="contaBox" style="border: 5px solid green;">
     <h1>
         <?php echo "<b>" . 'Nova Conta'  . "</b>" ?>
     </h1>
@@ -17,32 +17,24 @@
             <strong><?= $erroNew ?></strong>
         </div>
     <?php endif; ?>
-    <div class="row">
-        <div class='col' style="width: 200px">
-            <form action="index.php?acao=contas" method="POST">
-                <div class="form-group">
-                    <label>Nome da conta</label>
-                    <input class="form-control" type="text" name="novoNome" style="width: 400px">
-                </div>
-                <div class="form-group">
-                    <label>Login</label>
-                    <input class="form-control" type="text" name="novoLogin" style="width: 400px">
-                </div>
-                <div class="form-group">
-                    <label>Senha</label>
-                    <input class="form-control" type="text" name="novaSenha" id="senha" style="width: 270px">
-                </div>
-                <div class="row" style="padding-top: 15px">
-                    <div class="col">
-                        <button type="submit" class="btn btn-success btn-lg" style="font-size:25px; font-weight: bold;"><i class="fa fa-plus" style="padding-right: 20px"></i>Adicionar</button>
-                    </div>
-                </div>
-            </form>
+
+    <form action="index.php?acao=contas" method="POST">
+        <div class="form-group">
+            <label>Nome da conta</label>
+            <input class="form-control" type="text" name="novoNome">
         </div>
-        <div class='col' style='padding-top: 145px; padding-left: 60px'>
-            <button type="button" class="btn btn-primary" style="margin: 0px" onclick="change()">Gerar aleatória</button>
+        <div class="form-group">
+            <label>Login</label>
+            <input class="form-control" type="text" name="novoLogin">
         </div>
-    </div>
+        <label>Senha</label>
+        <div class="input-group mb-3">
+            <input class="form-control" type="text" name="novaSenha" id="senha">
+            <button type="button" class="btn btn-primary" style="margin: 0px" onclick="change()">Gerar Aleatória</button>
+        </div>
+        <button type="submit" class="btn btn-success btn-lg" style="font-size:25px; font-weight: bold;"><i class="fa fa-plus" style="padding-right: 20px"></i>Adicionar</button>
+    </form>
+
 </div>
 
 <br>
