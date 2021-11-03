1450ba1973d921a78d5c8a61f6f290cd54600d50
Matheus <matheusrigonigalvao@gmail.com>
2021-11-03 13:56:15 -0300

    script do banco

---
+++ script.sql
@@ -0,0 +1,22 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+
+
+
+CREATE TABLE Usuario (
+IDUsuario INTEGER PRIMARY KEY,
+Senha VARCHAR(1000),
+Sobrenome VARCHAR(1000),
+Nome VARCHAR(200),
+Email VARCHAR(1000)
+)
+
+CREATE TABLE Conta (
+IDConta INTEGER PRIMARY KEY,
+Senha VARCHAR(500),
+Nome VARCHAR(500),
+Login VARCHAR(1000),
+IDUsuario INTEGER,
+FOREIGN KEY(IDUsuario) REFERENCES Usuario (IDUsuario)
+)
+
